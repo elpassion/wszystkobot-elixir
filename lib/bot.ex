8471855477f76bbaf864397b7b2d5111767e239a
Rafał Lisowski <lisukorin@gmail.com>
2016-02-04 15:35:03 +0100

    readme

--- lib/bot.ex
+++ lib/bot.ex
@@ -4,6 +4,7 @@ defmodule Bot do
   def start(_type, _args) do
     IO.puts System.get_env("SLACK_TOKEN")
     Bot.start_link(System.get_env("SLACK_TOKEN"), [])
+    :timer.sleep(:infinity)
   end
 
   def handle_connect(slack, state) do
