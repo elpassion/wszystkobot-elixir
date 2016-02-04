087b278f9d3778efd307d4a366ed521d4a84875c
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-02-04 15:38:51 +0100

    Added test watcher mix. Added methods to parsing forwarder input.

--- message_forwarder/test/message_forwarder_test.exs
+++ message_forwarder/test/message_forwarder_test.exs
@@ -2,7 +2,15 @@ defmodule MFTest do
   use ExUnit.Case
   doctest MF
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "handle no input" do
+    assert MF.handle_input() == {:error, "No message received!"}
+  end
+
+  test "handle empty input" do
+    assert MF.handle_input("") == {:error, "Input is empty. No message received!"}
+  end
+
+  test "handle input" do
+    assert MF.handle_input("<@U084BDT55>: asd") == {:ok, "U084BDT55", "asd"}
   end
 end
