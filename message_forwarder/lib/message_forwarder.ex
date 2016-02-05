e24fa09840402e9950634896bcd4793d3f9d7599
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-02-05 16:13:03 +0100

    Added condition for handling ingored messages. Code refactor. Test update

--- message_forwarder/lib/message_forwarder.ex
+++ message_forwarder/lib/message_forwarder.ex
@@ -5,22 +5,27 @@ defmodule MessageForwarder do
     Profanity.new
   end
 
-  def handle_input() do
+  def forward_message() do
     {:error, "No message received!"}
   end
 
-  def handle_input("") do
+  def forward_message("") do
     {:error, "Input is empty. No message received!"}
-  end
-
-  def handle_input(input) do
-    [matched_string, user_name] = Regex.run(~r/^<@([A-Z]\w+)>:/, input)
-    message = input |> String.replace(matched_string, "") |> String.strip
-    IO.puts Profanity.censor_profane(message)
-    {:ok, :message, message, user_name}
   end
 
   def forward_message(input) do
     handle_input(input)
+  end
+
+  defp handle_input(input) do
+    [matched_string, user_name] = Regex.run(~r/^<@([A-Z]\w+)>:/, input)
+    case user_name do
+      nil ->
+        {:ignored, :none, ""}
+      _ ->
+        message = input |> String.replace(matched_string, "") |> String.strip
+        message = Profanity.censor_profane(message)
+        {:ok, :message, message, user_name}
+    end
   end
 end
