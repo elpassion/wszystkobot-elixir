50fdd743b7d81dd1fbadb18b459356ba22184050
Mariusz Kulerski <mariusz.kulerski@elpassion.pl>
2016-02-06 10:49:01 +0100

    Fix indentation in Calc module

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -1,7 +1,7 @@
 defmodule Calc do
-	@moduledoc false
+  @moduledoc false
 
-	import List
+  import List
 
   @servicable_msg_regexp ~r/^How much is ([0-9+-\/*%\(\) ]*)/i
   @fail_msg "Naah, something is wrong with your equation, man."
@@ -18,16 +18,16 @@ defmodule Calc do
     end
   end
 
-	def can_handle_message?(msg) do
-		@servicable_msg_regexp |> Regex.match?(msg)
-	end
+  def can_handle_message?(msg) do
+    @servicable_msg_regexp |> Regex.match?(msg)
+  end
 
-	defp extract_equation(msg) do
-		@servicable_msg_regexp |> Regex.run(msg) |> last
-	end
+  defp extract_equation(msg) do
+    @servicable_msg_regexp |> Regex.run(msg) |> last
+  end
 
-	defp calculate(equation) do
-		equation |> Expr.eval! |> hd
-	end
+  defp calculate(equation) do
+    equation |> Expr.eval! |> hd
+  end
 
 end
