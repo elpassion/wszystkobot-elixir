a7a5b3f8b34c849799740c41b7f934cc5dee5618
Rafał Lisowski <lisukorin@gmail.com>
2016-02-11 15:05:32 +0100

    add help to calc and hub

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -6,7 +6,9 @@ defmodule Calc do
 
   import List
 
+  match ~r/help/i, :help
   match ~r/^How much is ([0-9+-\/*%\(\) ]*)/i, :interact
+  match ~r/^calc ([0-9+-\/*%\(\) ]*)/i, :interact
 
   def interact(calc, msg, equation) do
     try do
@@ -22,7 +24,19 @@ defmodule Calc do
     |> Kernel.to_string
   end
 
+  def help(reporter, message) do
+    say reporter, message["channel"], help_message
+  end
+
   defp calculate(equation) do
     equation |> Expr.eval! |> hd
+  end
+
+  defp help_message do
+    ~s"""
+      calc:
+      	how much is OP - returns the result of a mathematical operation
+      	calc OP - returns the result of a mathematical operation
+    """
   end
 end
--- hub_reporter/lib/hub_reporter.ex
+++ hub_reporter/lib/hub_reporter.ex
@@ -4,11 +4,16 @@ defmodule HubReporter do
   use Slacker
   use Slacker.Matcher
 
+  match ~r/help/i, :help
   match ~r/^hub token (.*)/i, :store_token
   match ~r/^hub token\z/i, :show_token
   match ~r/^hub fetch\z/i, :fetch
   match ~r/^hub status\z/i, :status
   match ~r/^hub push ?(.*)/i, :push
+
+  def help(reporter, message) do
+    say reporter, message["channel"], help_message
+  end
 
   def show_token(reporter, message) do
     say reporter, message["channel"], TokenHandler.handle(message["user"])
@@ -28,5 +33,16 @@ defmodule HubReporter do
 
   def push(reporter, message, report) do
     say reporter, message["channel"], ReportsHandler.handle_sending_report(report, message["user"])
+  end
+
+  defp help_message do
+    ~s"""
+    hub:
+    	hub token       - shows user token
+    	hub token TOKEN - saves user token
+    	hub fetch       - shows reports from last 7 days
+    	hub status      - shows last report
+    	hub push MSG    - creates new time report similar to last one with MSG
+    """
   end
 end
