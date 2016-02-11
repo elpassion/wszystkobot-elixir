240ce91df0825cd88f6bf6d81e515d007f519aeb
Rafał Lisowski <lisukorin@gmail.com>
2016-02-11 14:49:45 +0100

    simpler calc module

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -6,23 +6,20 @@ defmodule Calc do
 
   import List
 
-  @servicable_msg_regexp ~r/^How much is ([0-9+-\/*%\(\) ]*)/i
+  match ~r/^How much is ([0-9+-\/*%\(\) ]*)/i, :interact
 
-  match @servicable_msg_regexp, :interact
-
-  def interact(calc, msg, level) do
-    say calc, msg["channel"], proceed(msg["text"])
+  def interact(calc, msg, equation) do
+    try do
+      say calc, msg["channel"], proceed(equation)
+    rescue
+      _ -> say calc, msg["channel"], "Naah, something is wrong with your equation, man."
+    end
   end
 
   def proceed(msg) do
     msg
-    |> extract_equation
     |> calculate
     |> Kernel.to_string
-  end
-
-  defp extract_equation(msg) do
-    @servicable_msg_regexp |> Regex.run(msg) |> last
   end
 
   defp calculate(equation) do
