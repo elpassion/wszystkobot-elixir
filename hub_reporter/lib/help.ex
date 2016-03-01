7b8da2a35c452d479ed1f5c9b1e6b7ded9c00bf8
Karol Wyliziński <k.wylizinski@wp.pl>
2016-03-01 12:47:37 +0100

    edit commands in help

--- hub_reporter/lib/help.ex
+++ hub_reporter/lib/help.ex
@@ -4,8 +4,11 @@ defmodule HubReporter.Help do
       hub token TOKEN - saves user token
       hub token - shows user token
       hub status - shows a status for a simplified hub push command
-      hub push COMMENT - adds a time report for today's date, 8 hours and today's date
-      hub push YYYY-MM-DD | HOURS | COMMENT - adds a time report for latest project with declared date and value, for example: "hub push 2000-10-10 | 8 | playing fifa all day long" (USE THIS ORDER!)
+      hub push COMMENT - adds a time report for 8 hours and today's date
+      hub alias - shows your aliases
+      hub alias ALIAS | ID - adds new alias for specific projects id
+      hub projects - shows projects ids
+      hub push YYYY-MM-DD | HOURS | ALIAS | COMMENT - adds a time report with declared date, value and project, for example: "hub push 2000-10-10 | 8 | OEL | playing fifa all day long" (USE THIS ORDER!)
     """
   end
 end
